3a133f0960f23be28e984b2d184ea391ba27613c
Dennnhhhickk <iseleh09@gmail.com>
2022-03-20 03:46:13 +0300

    Upgrade checker

--- checker.sh
+++ checker.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-bash build.sh
+if ! [ -f './a.out' ]
+then
+    bash build.sh
+else
+    _cacrl=`date +%s -r a.out`
+    _oldcrl=`date +%s -r main.cpp`
+
+    if (( $_cacrl - $_oldcrl < 0 ))
+    then 
+        bash build.sh
+    fi
+fi
 
 cd tests/in
 
@@ -8,7 +19,8 @@ for filename in *
 do
     ../../a.out <$filename >../out/$filename
 
-    if test -f ../ans/$filename; then
+    if test -f ../ans/$filename
+    then
         diff ../out/$filename ../ans/$filename || echo Error: Wrong answer on test $filename
     else
         echo Warning: No correct answer on test $filename
